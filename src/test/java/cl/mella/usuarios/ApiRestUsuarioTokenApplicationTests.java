e3ddd40ae504825cda7fb3f8ca7d5d9daf8fb900
lmella <luis.redd>
2021-02-21 22:18:51 -0300

    Incorporacion de roles en usuarios

--- src/test/java/cl/mella/usuarios/ApiRestUsuarioTokenApplicationTests.java
+++ src/test/java/cl/mella/usuarios/ApiRestUsuarioTokenApplicationTests.java
@@ -16,47 +16,4 @@ import java.util.regex.Pattern;
 class ApiRestUsuarioTokenApplicationTests {
 
 
-    @Test
-    void contextLoads() {
-        String regex = "^(?=(?:.*\\d){2})(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})([A-Za-z\\d])(?=\\S+$)";
-        //Pattern pat = Pattern.compile(regex);
-        Pattern pat = Pattern.compile(regex);
-        String input = "Hola 11";
-        //Matcher mat = pat.matcher(input);
-        Matcher mat = pat.matcher(input);
-        if (mat.find()) {
-            log.info("regex encontrada");
-            //log.info("Sujeto activo :"+mat.group(1));
-            //log.info("Sujeto pasivo :"+mat.group(2));
-        } else {
-            log.info("regex NO encontrada");
-        }
-        //log.info("hola");
-    }
-
-    @Test
-    public void grego() {
-
-        log.info(GregorianCalendar.getInstance().getTime());
-    }
-
-
-    @Test
-    public void extraerEmail() {
-        UUID uuid = UUID.randomUUID();
-        Registro registro = new Registro(uuid,
-                "Lmella@gmail.com",
-                "PassLuismella",
-                "12345ABCDE",
-                true);
-
-
-        RetRegistro ret = new RetRegistro(registro);
-
-        log.info(ret);
-    }
-
-
-
-
 }
